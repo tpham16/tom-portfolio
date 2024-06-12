02e175d05a4acb6df5b1fa97388a3f7963728c97
Tom Pham <teepham16@gmail.com>
2024-06-12 11:52:04 -0700

    resume

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,44 +1,49 @@
-"use client" // this is a client component
-import React from "react"
-import { useState } from "react"
-import { Link } from "react-scroll/modules"
-import { usePathname } from "next/navigation"
-import { useTheme } from "next-themes"
-import { RiMoonFill, RiSunLine } from "react-icons/ri"
-import { IoMdMenu, IoMdClose } from "react-icons/io"
+"use client"; // this is a client component
+import React from "react";
+import { useState } from "react";
+import Link from "next/link"; // Import the Link component from next/link
+import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
+import { RiMoonFill, RiSunLine } from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 
 interface NavItem {
-  label: string
-  page: string
+  label: string;
+  page: string;
 }
 
 const NAV_ITEMS: Array<NavItem> = [
   {
     label: "Home",
-    page: "home",
+    page: "#home",
   },
   {
     label: "About",
-    page: "about",
+    page: "#about",
   },
   {
     label: "Projects",
-    page: "projects",
+    page: "#projects",
   },
-]
+  {
+    label: "Resume",
+    page: "/resume.pdf",
+  },
+];
 
 export default function Navbar() {
-  const { systemTheme, theme, setTheme } = useTheme()
-  const currentTheme = theme === "system" ? systemTheme : theme
-  const pathname = usePathname()
-  const [navbar, setNavbar] = useState(false)
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const pathname = usePathname();
+  const [navbar, setNavbar] = useState(false);
+
   return (
     <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
         <div>
           <div className="flex items-center justify-between py-3 md:py-5 md:block">
-            <Link to="home">
-              <div className="container flex items-center space-x-2">
+            <Link href="#home">
+              <div className="container flex items-center space-x-2 cursor-pointer">
                 <h2 className="text-2xl font-bold">Tom Pham</h2>
               </div>
             </Link>
@@ -61,23 +66,30 @@ export default function Navbar() {
           >
             <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
               {NAV_ITEMS.map((item, idx) => {
-                return (
-                  <Link
-                    key={idx}
-                    to={item.page}
-                    className={
-                      "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100 cursor-pointer"
-                    }
-                    activeClass="active"
-                    spy={true}
-                    smooth={true}
-                    offset={-100}
-                    duration={500}
-                    onClick={() => setNavbar(!navbar)}
-                  >
-                    {item.label}
-                  </Link>
-                )
+                if (item.page.endsWith(".pdf")) {
+                  return (
+                    <a
+                      key={idx}
+                      href={item.page}
+                      className="block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-100 cursor-pointer"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {item.label}
+                    </a>
+                  );
+                } else {
+                  return (
+                    <Link key={idx} href={item.page}>
+                      <span
+                        className="block lg:inline-block text-neutral-900 hover:text-neutral-500 dark:text-neutral-100 cursor-pointer"
+                        onClick={() => setNavbar(!navbar)}
+                      >
+                        {item.label}
+                      </span>
+                    </Link>
+                  );
+                }
               })}
               {currentTheme === "dark" ? (
                 <button
@@ -99,5 +111,5 @@ export default function Navbar() {
         </div>
       </div>
     </header>
-  )
+  );
 }
